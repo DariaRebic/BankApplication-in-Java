f188fffa6d0fcca595c72cecfedbf595cbf1475c
DariaRebic <151089680+DariaRebic@users.noreply.github.com>
2023-11-19 18:51:37 +0100

    BankingApplication.java

--- BankingApplication.java
+++ BankingApplication.java
@@ -1,34 +1,42 @@
+//This line imports the Scanner class from the java.util package. The Scanner class is used for taking user input.
 import java.util.Scanner;
 
 public class BankingApplication {
 	public static void main(String[] args) {
+		//an instance of the BankAccount class is created with the name obj and initialized
 		BankAccount obj = new BankAccount("Daria", "101010");
+		//The ShowMenu method of the BankAccount class is called on the obj instance.
 		obj.ShowMenu();
 	}
 }
 class BankAccount{
+	//variables 
 	int balance;
 	int previousTransaction;
 	String custumerName;
 	String custumerId;
-	
+
+	//constructor, it initializes the customer name and customer ID when an object of this class is created
 	 BankAccount(String tName, String tId) {
 		 custumerName = tName;
 		 custumerId = tId;
 	}
-	
+
+	//method 
 	void deposit(int ammount) {
 		if (ammount != 0) {
 			balance = balance + ammount;
 			previousTransaction = ammount;
 		}
 	}
+	//method 
 	void withdraw(int ammount) {
 		if (ammount != 0) {
 			balance = balance - ammount;
 			previousTransaction = -ammount;
 		}
 	}
+	//method
 	void getPreviousTransaction() {
 		if(previousTransaction > 0) {
 			System.out.println("Deposited: " + previousTransaction); 
@@ -40,8 +48,11 @@ class BankAccount{
 			System.out.println("No Transaction Occured.");
 		}
 	}
+	//method, represents the main menu of the banking application
 	void ShowMenu() {
-		char option = '\0';
+		char option = '\0'; 
+		
+		//the method initializes a Scanner object for user input
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.println("Welcome " + custumerName);
@@ -53,16 +64,19 @@ class BankAccount{
 		System.out.println("C : Withdraw");
 		System.out.println("D : Previous Transaction");
 		System.out.println("E : Exit");
-		
+
+		//the method uses a do-while loop to repeatedly display the menu until the user chooses to exit
 		do {
 			System.out.println("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
 			System.out.println("Enter Your Option");
 			System.out.println("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
+			// Menu display and input handling
 			option = scanner.next().charAt(0);
 			
 			System.out.println("\n");
 			
 			switch(option) {
+			// Menu options handling
 			case 'A':
 				System.out.println("--------------------------------------------------------------------------------");
 				System.out.println("Balance = " + balance);
@@ -107,6 +121,7 @@ class BankAccount{
 			}
 		}
 		while (option != 'E');
+		//closes the ShowMenu method and the BankAccount class.
 		System.out.println("Thank you for using our services :)"); 
 	}
 }
